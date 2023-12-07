e94c39702ac2eca78617157fc059c9b9bc7704eb
mehmet dogan <mehmetseymen91@gmail.com>
2023-12-07 21:29:20 +0300

    bootstrap import edildi

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
+import CardContainer from "./components/CardContainer";
+import Header from "./components/Header";
+import Container from "react-bootstrap/Container";
 
 function App() {
   return (
-    <div>Hello React</div>
+    <Container className="text-center mt-4">
+      <Header />
+      <CardContainer/>
+    </Container>
   );
 }
 
---
+++ src/components/CardContainer.jsx
@@ -0,0 +1,28 @@
+import Container from "react-bootstrap/Container";
+import Form from "react-bootstrap/Form";
+import Row from "react-bootstrap/Row";
+// import Card from "react-bootstrap/Card";
+import Col from "react-bootstrap/Col";
+import { data } from "../helpers/data";
+import PlayerCard from "./PlayerCard";
+
+const CardContainer = () => {
+  console.log(data);
+  return (
+    <>
+      <Form.Control placeholder="Search a player..." className="w-50 m-auto" />
+
+      <Container className="rounded-4 my-4 p-3 card-container">
+        <Row>
+          {data.map((player, i) => (
+            <Col xl={3} lg={4} md={6}>
+            <PlayerCard {...player}/>
+            </Col>
+          ))}
+        </Row>
+      </Container>
+    </>
+  );
+};
+
+export default CardContainer;
